ca5f86e4288bc8444d93b60feb061cdd85e0d4b2
Sebastian Thadewald <bo141@Sebastians-iMac.local>
2020-01-13 15:13:57 +0100

    added new bullshit

---
+++ test/headlineGen.js
@@ -0,0 +1,96 @@
+randomWords = {};
+randomWords["buzz"] = "Angst,Angstzustand,Auflösung,Aufregung,Erschrecken,Heidenangst,Höllenangst,Konfusion,Kopflosigkeit,Panik,Schreck,Schrecken,Unruhe,Verwirrung,Bestürzung,Entsetzen,Erschrockenheit,Erschütterung,Fassungslosigkeit,Grauen,Grausen,Horror,Panik,Schauder,Schreck,Schrecken,Verwirrung,Beschämung,Blamage,Bloßstellung,Demütigung,Entehrung,Erniedrigung,Kompromittierung,Kränkung,Schande,Schandfleck,Schandmal,Schimpf,Unehre,Verruf,Aufruhr,Aufsehen,Beachtung,Eklat,Furore,Hallo,Klamauk,Lärm,Radau,Sensation,Tamtam,Verwirrung,Zirkus"
+randomWords["headline"] = "Abschiebe-Mädchen,Altersangabe,Aschenputtel-Richterin,anonyme Handy-Nachricht,Ausbrech-Bär,Baby-Beweis,Balken-Fußball,Balkonmonster,Bauch-Geheimnis,Bein-ab-Professor,Benz-Baby,Benzin-Wut,Bestie,Bin-Laden-Vize,Blut-Rallye,Blutschande-Kinder,Blut-Vermögen,Boris Specker,Busen-Alarm,Busen-Ballarina,Busenblitzalarm,Busenmacher-Witwe,Busen-Krieg,Busen-Witwe,China-Sklaven,David Gagahoff,Deutürken,Deutürk-Fahne,Döner-Killer,Drogen-Attacke auf dem Schulhof,Dutten,Eis-Baby,Eis-Eltern,Eis-Halle,Eis-Mädchen,Eis-Mutter,Eis-Vater,Energie-Abzocker,Erb-Penner,Erdnuss-Komet,Flucht-Affe,folgenlos Beschuldigter,Fremdkuss,Ganz Deutschland,Gasnosse,Glatzkopf-Killer,Gluck-gluck-weg-war'n-sie-Schwimmer,Google-Killer,Grand-Prix-Porno,Grinse-Killer,Grinsi-Klinsi,Grusiker,Gyros-Bomber,Hamster-Dödel,Haschisch-Bomber,Hunger-Hund,HunSticker,Michelle,Freie Sicht auf die Hupen,Hurensohn,Igelmädchen,Inzest-Paar,Irre von Teheran,Jahrtausend-Papst,Jammer-Buch,Käfigbande,Kaiser-Heli,Käse-Tussi,Katzen-Kraft,Killer-Krankheit,Klau-Chance,Klümchen,Klümchen Nr. 2,Klum-Kugel,Krawall-Bär,Kuss-Unfall,Krebsjunge,Kuckucks-Vater (auch Zahle-Papa),Kurti (auch faltiger Kurti),Kurvikova,Leichenprofessor,Liebesdepp,Deutschlands größter,Liebesmonster,Mini-Models,Maulkorb-Urteil,Mini-Enrique,Mord-Reiter,Müll-Kinder,nachrechnen,Nackt-Freundin,Nackt-Fotograf,Nackt-Test,Nackt-Unfall,Nackt-Tochter,Nackt-Zensur,Narbenmann,Nippel-Alarm,Nippel-News,Ohr-ab-Hund,Ohr-Abschneider,Papstneidische Glaubensbrüder,Pasta-Schnute,Penis-Attacke,Penis-Opfer,Perversling,Pfui-Liga,Pfui-Profis,Pfui-Skandal,Pfui-TV,Pfusch-Mädchen,Pilzminator,Pipi-Fälschung,Pipi-Überwachung,Pocasso,Pop-Titan,Popo-Alarm,Popo-Unfall,Porno-Rückfall,Schmuddel-Rückfall,Porno-Verdacht,Prinzessin von Hohenkullern,Prügelnacht von Istanbul,Prügel-Nacht von Potsdam,Prügel-Prinz,Prügel-Türke,Pummelnaldo,Pummel-Tore,Reifen-Depp,renommiert,Rollstuhl-Mädchen,Rollstuhl-Sängerin,Sahara-Sex,SAT 1-Baby,Schamlos-Ehepaar,Schaumglocken,Schlechtschreibreform,Schleck-Fall,Schlüpfer-Opa,schmidtzig,Schnief-Viren,Schnitzel-Stefan,Schummel-Huren,Schummel-Pinkler,See-Schlacht,Sekt-Anschlag,Sekt-Politiker (→ Sekt-Anschlag),Sendezeit schinden,sexeln,Sex-Erpressung,Sex-Hure,Sing-Unfall,Single,SMSeln,Super-Polizist,SNN,Suppen-Sylvie,Tanga-Grapscher,Tanga-Terror,Telefonbusen,Terror-Witwe,Todespfleger,Totraser,Tot-Spritzer,Tränenjunge (auch Tränenkind),Tsunami-Kind,Türkei-Virus,Türken-Sünder,TV-Titan,Unterpfotung,Umzugskisten-Gorilla,Venus-Geweih,Wasser-Schweini,Riesen-Wirbel,Sex-Wirbel,Liebes-Wirbel,Porno-Wirbel,Baby-Wirbel,Zisch-und-weg-Polo,Zombie-Killer,Zuckertüte,Zwischen Schambein und Schaumglocken"
+randomWords["praepos"] = "von,aus,in"
+randomWords["place"] = "Baltrum,Fraueninsel,Helgoland,Herreninsel,Juist,Insel Hiddensee,Langeoog,Neuwerk,Spiekeroog,Wangerooge,Tokio-Yokohama,Delhi,Shanghai,São Paulo,Mumbai,Mexiko,Peking,Osaka,Kairo,New York,Istanbul"
+randomWords["verbs"] = "schlägt,wäscht,bläst,empfängt,liest,empfiehlt,gebiert,isst,frisst,tritt,bricht,erschrickt,nimmt,trifft,verdirbt"
+randomWords["names"] = "Fahrrad,Feuerlöscher,Film,Foto,Freiheit,Gehirn,Gehweg,Grundgesetz,Grundstück,Gymnasium,Hafen,Haus,Heimatland,Holz,Horn,Igel,Impfstoff,Information,Infusion,Insel,Jachthafen,Jacke,Jäger,Jobcenter,Jugendclub,Kaktus,Kamm,Kammer,Keller,Kugel,Leber,Leiste,Leiter,Liebe,Locher,Maus,Monat,Monitor,Musikstück,Muskel,Nabelschnur,Nachbar,Nagel,Nase,Natur,Nonne,Notunterkunft,Obst,Ochse,Offizier,Orgel,Osterei,Bruno Ganz,Hannelore Elsner,Karl Lagerfeld,Doris Day,Sigmund Jähn,Rudi Assauer,Adolf Hitler,Angela Merkel,Donald Trump,Nina Hagen,Dominik Weiss,Tobias Scholz,Alexander Henrichsen,Sebastian Thadewald,Thorsten Frings,Markus Söder,Michael Müller,Reiner Haseloff,Malu Dreyer,Olaf Scholz,Horst Seehofer,Heiko Maas,Apache 207,Nimo,Tyga,Mark Forster,Billie Eilish,Loredana"
+
+
+
+
+$(document).ready(function() {
+    if (localStorage.getItem('items') === null) {
+        localStorage.setItem("items", "");
+    } else {
+        $.each(localStorage.getItem('items').split(','), function(i,v) {
+            $('.old-results').append('<div>'+v+'</div>')
+        })
+    }
+
+    $('#result').html('');
+    counter = 0;
+    $.each(randomWords, function(i,v) {
+        console.log(i);
+        content = v.split(',')[Math.floor(Math.random()*v.split(',').length)]
+        if(i == "buzz"){
+            var container = '<div class="d-flex flex-wrap flex-column justify-content-start headliner" id="'+i+'"><div class="content">'+content+'!</div> <div class="controls"><a href="#" class="next roll">Next!</a></div> </div>'
+            counter++
+        }else if(i == 'headline') {
+            var container = '&nbsp;<div class="d-flex flex-wrap flex-column justify-content-start headliner" id="'+i+'"><div class="content">"'+content+'"</div> <div class="controls"><a href="#" class="next roll">Next!</a></div> </div>'
+            counter++
+        }else {
+            var container = '&nbsp;<div class="d-flex flex-wrap flex-column justify-content-start headliner" id="'+i+'"><div class="content">'+content+'</div> <div class="controls"><a href="#" class="next roll">Next!</a></div> </div>'
+        }
+        $('#result').append(container)
+    })
+    counter = 0;
+
+    var sentence = "";
+
+    $('.content').each(function(){
+        sentence += $(this).text() + " "
+    })
+
+    storageItem = localStorage.getItem("items")
+    if( storageItem.length > 0) {
+        storageItem=storageItem+', '
+    }
+
+    localStorage.setItem('items', storageItem + sentence)
+
+    $.each(localStorage.getItem('items').split(','), function(i,v) {
+        $('.old-results').append('<div>'+v+'</div>')
+    })
+
+
+    ul = $('.old-results'); // your parent ul element
+    ul.children().each(function(i,li){ul.prepend(li)})
+    $('body').on('click', 'a.next.roll', function() {
+        id = $(this).parent().parent().attr('id');
+
+        console.log(id);
+
+        content = randomWords[id].split(',')[Math.floor(Math.random()*randomWords[id].split(',').length)]
+        if(id == "buzz") {
+            var html = '<div class="content">'+content+'!</div><div class="controls"><a href="#" class="next roll">Next!</a></div> '
+        } else if(id == "headline") {
+            var html = '<div class="content">"'+content+'"</div><div class="controls"><a href="#" class="next roll">Next!</a></div> '
+        } else {
+            var html = '<div class="content">'+content+'</div><div class="controls"><a href="#" class="next roll">Next!</a></div> '
+        }
+        $('#'+id).html(html)
+        var sentence = "";
+
+        $('.content').each(function(){
+            sentence += $(this).text() + " "
+        })
+
+        storageItem = localStorage.getItem("items")
+        if( storageItem.length > 0) {
+            storageItem=storageItem+', '
+        }
+        localStorage.setItem('items', storageItem + sentence)
+
+
+        $('.old-results').html('')
+
+        $.each(localStorage.getItem('items').split(','), function(i,v) {
+            $('.old-results').append('<div>'+v+'</div>')
+        })
+
+
+        ul = $('.old-results'); // your parent ul element
+        ul.children().each(function(i,li){ul.prepend(li)})
+    })
+})
